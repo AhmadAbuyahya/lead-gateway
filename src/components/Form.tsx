d888fc4aebf8e8ca0230f5695aa6732ff72e61f2
Ahmad Abuyahya <ahmadabuyahya998@gmail.com>
2024-07-30 10:42:39 +0300

    initialize project

---
+++ src/components/Form.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const Form = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        setCount(count + 1);
+      }}
+    >
+      <input type="text" value={count} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default Form;
